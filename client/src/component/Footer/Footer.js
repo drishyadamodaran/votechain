335262bbe52c0802cc42a096be6b63752914c84a
drishyadamodaran <56961626+drishyadamodaran@users.noreply.github.com>
2022-06-23 07:58:22 +0000

    Add files via upload

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          View this project on{" "}
+          <a
+            className="profile"
+            href="https://github.com/drishyadamodaran/votechain"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
